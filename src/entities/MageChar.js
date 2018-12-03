9568a312548341316a00a26865138cb4bc2e1ae3
Joshua Anthony <jantho1990@gmail.com>
2018-12-03 13:18:01 -0600

    downloaded some potential assets; exported mage animations to a separate file

--- src/entities/MageChar.js
+++ src/entities/MageChar.js
@@ -4,6 +4,7 @@ import SanityCaster from '../systems/SanityCaster'
 import Container from '../../titus/Container'
 import SoundPool from '../../titus/sound/SoundPool'
 import { GameData } from '../../titus/Game';
+import { animations } from '../../resources/characters/mage/sara-cal-animations'
 
 const texture = new Texture('resources/characters/mage/sara-cal.png', (t, e) => {
   if (t.img.src.indexOf('data:image') === -1) {
@@ -13,32 +14,6 @@ const texture = new Texture('resources/characters/mage/sara-cal.png', (t, e) => 
 
 const sounds = {
   jump: new SoundPool('resources/sounds/jump.wav')
-}
-
-const animations = {
-  'stand': [
-    { x: 0, y: 8, w: 32, h: 48 }
-  ],
-  'slap': [
-    { x: 36, y: 8, w: 32, h: 48 },
-    { x: 72, y: 8, w: 32, h: 48 },
-    { x: 108, y: 8, w: 32, h: 48 }
-  ],
-  'bow': [
-    { x: 188, y: 72, w: 40, h: 48 },
-    { x: 232, y: 72, w: 40, h: 48 },
-    { x: 276, y: 72, w: 40, h: 48 }
-  ],
-  'dying': [
-    { x: 36, y: 136, w: 32, h: 48 },
-    { x: 72, y: 136, w: 32, h: 48 },
-    { x: 112, y: 152, w: 56, h: 32 }
-  ],
-  'walk': [
-    { x: 0, y: 72, w: 32, h: 48 },
-    { x: 36, y: 72, w: 32, h: 48 },
-    { x: 72, y: 72, w: 32, h: 48 }
-  ]
 }
 
 
